bb1033fbc634ecb4eaf2810c5f3c92d9c062f406
Khaja Minhajuddin <minhajuddink@gmail.com>
2018-11-29 13:26:37 -0500

    init

---
+++ lib/uuid_int.ex
@@ -0,0 +1,18 @@
+defmodule UuidInt do
+  @moduledoc """
+  Documentation for UuidInt.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> UuidInt.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/uuid_int_test.exs
@@ -0,0 +1,8 @@
+defmodule UuidIntTest do
+  use ExUnit.Case
+  doctest UuidInt
+
+  test "greets the world" do
+    assert UuidInt.hello() == :world
+  end
+end
